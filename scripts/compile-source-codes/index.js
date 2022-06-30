c98abf2db9561c8181037c7e120e839a36fc98b0
billy laing <billy.laing@trestle.us>
2022-06-30 15:10:24 -0400

    [Facebook] Add domain verification

--- scripts/compile-source-codes/index.js
+++ scripts/compile-source-codes/index.js
@@ -3,18 +3,9 @@ const Airtable = require("airtable");
 const base = new Airtable().base("appc14jHeQ2v7FhU9");
 
 const getApprovedRecords = async () => {
-   // const filterByFormula = "{status} = 'Approved'"
-   // const fields = ["organization", "source_code", "status"];
-   // return base("Partners").select({ filterByFormula, fields }).all();
-   return Promise.resolve([
-      {
-         fields: {
-            organization: "billy test org",
-            source_code: "billy",
-            status: "Approved",
-         },
-      },
-   ]);
+   const filterByFormula = "{status} = 'Approved'"
+   const fields = ["organization", "source_code", "status"];
+   return base("Partners").select({ filterByFormula, fields }).all();
 };
 
 const getSourceCodes = (partner) => {
