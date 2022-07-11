e4f9b3d5b309a258d39b431e64d6fe86f9b0309e
billy laing <billy.laing@trestle.us>
2022-07-11 10:26:03 -0400

    [Automation] Add tests for Partner List aka source codes json

--- scripts/compile-source-codes/index.test.js
+++ scripts/compile-source-codes/index.test.js
@@ -3,16 +3,26 @@ test("Partner List loads correctly", () => {
    expect(partnerList.length).not.toBe(0);
 });
 
+const getSourceCodes = (partnerList) => {
+   return partnerList.reduce((result, partner) => {
+      if (partner.additionalVanityUrls) {
+         return [
+            ...result,
+            partner.partnerId,
+            ...partner.additionalVanityUrls,
+         ];
+      } else {
+         return [...result, partner.partnerId];
+      }
+   }, []);
+};
+
 test("Partner List does not have duplicate source codes", () => {
-   const partnerList = require("../../site/src/data/PartnerList.json")
-   const findDuplicateSourceCode = () => {
+   const partnerList = require("../../site/src/data/PartnerList.json");
+   const sourceCodes = getSourceCodes(partnerList);
+   const lowerSourceCodes = sourceCodes.map((sourceCodes) => sourceCodes.toLowerCase())
+   const findDuplicates = (array) => array.filter((item, index) => array.indexOf(item) != index)
+   const duplicateElements = findDuplicates(lowerSourceCodes)
+   expect(duplicateElements).toHaveLength(0)
+});
 
-   }
-})
-
-test("Partner List does not have duplicate org names", () => {
-   const partnerList = require("../../site/src/data/PartnerList.json")
-   const findDuplicateOrgNames = () => {
-
-   }
-})
