2f310a1fdd8a540b15d87592555927073c88e855
billy laing <billy.laing@trestle.us>
2022-07-11 12:23:45 -0400

    [Automation] Pull list of Action Kit Reports

--- scripts/create-partner-reports/index.js
+++ scripts/create-partner-reports/index.js
@@ -1,5 +1,9 @@
-const Airtable = require("airtable");
+import fetch, { Headers } from "node-fetch";
+import Airtable from "airtable";
+
 const base = new Airtable().base("appc14jHeQ2v7FhU9");
+const actionKitURL = "https://ptp.actionkit.com/rest";
+const { ACTION_KIT_USERNAME, ACTION_KIT_PASSWORD } = process.env;
 
 const getApprovedRecords = async () => {
    const filterByFormula = "{status} = 'Approved'";
@@ -15,13 +19,39 @@ const getApprovedRecords = async () => {
 };
 
 const getActionKitReportList = async () => {
-   return [];
+   const headers = new Headers();
+   headers.set(
+      "Authorization",
+      `Basic ${Buffer.from(
+         `${ACTION_KIT_USERNAME}:${ACTION_KIT_PASSWORD}`
+      ).toString("base64")}`
+   );
+
+   const res = await fetch(
+      `${actionKitURL}/v1/queryreport?categories__name=partners&_limit=100`,
+      {
+         headers,
+      }
+   );
+
+   if (!res.ok) {
+      throw new Error(
+         `HTTP Error Response: ${response.status} ${response.statusText}`
+      );
+   }
+
+   const response = await res.json();
+   if (response.meta.next) {
+      throw new Error("More than 100 reports returned!!!");
+   }
+
+   return response.objects;
 };
 
 const run = async () => {
    // get all approved partners
-   const approvedRecords = await getApprovedRecords();
-   console.log(approvedRecords.map(({ fields }) => fields));
+   // const approvedRecords = await getApprovedRecords();
+   // console.log(approvedRecords.map(({ fields }) => fields));
 
    // get partner reports from ActionKit
    const reportList = await getActionKitReportList();
