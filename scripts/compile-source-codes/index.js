4191dd987fe4b9624b13f2284b0188fabd62757c
Billy Laing <billy@powerthepolls.org>
2022-08-01 15:56:37 -0400

    Automate partner reports (#87)
    * [Automation] Pull all partner reports from ActionKit
    * [Automation] Create a PII report
    * [Automation] Add support for multiple source codes
    * [Automation] Add 'state' to PII report
    * [Automation] Add aggregate report
    * [Automation] Update report's name
    * [Refactor] Make airtable base an environment variable
    * [Automation] Add topline for aggregate report
    * [Automation] Report publishing complete, publish first new partner only as a test
    * [Refactor] Use getter instead of direct access on AirTable Record
    * [Automation] Use "report_status" field for approval
    * [Partner Reports] Automate partner reports and log body of any error messages

--- scripts/compile-source-codes/index.js
+++ scripts/compile-source-codes/index.js
@@ -1,23 +1,30 @@
 const fs = require("fs/promises");
 const Airtable = require("airtable");
-const base = new Airtable().base("appc14jHeQ2v7FhU9");
 
 const getApprovedRecords = async () => {
+   const { AIRTABLE_PARTNERS_BASE } = process.env;
+   const base = new Airtable().base(AIRTABLE_PARTNERS_BASE);
+
    const filterByFormula = "{status} = 'Approved'";
    const fields = ["organization", "source_code", "status"];
+
    return base("Partners").select({ filterByFormula, fields }).all();
 };
 
 const getSourceCodes = (partner) => {
-   return (partner.additionalVanityUrls
-      ? [partner.partnerId, ...partner.additionalVanityUrls]
-      : [partner.partnerId]).map(code => code.toLowerCase());
+   return (
+      partner.additionalVanityUrls
+         ? [partner.partnerId, ...partner.additionalVanityUrls]
+         : [partner.partnerId]
+   ).map((code) => code.toLowerCase());
 };
 
 const findDuplicates = (records, partnerList) => {
    return records.reduce((result, record) => {
       const duplicate = partnerList.find((partner) =>
-         getSourceCodes(partner).includes(record.fields.source_code.toLowerCase())
+         getSourceCodes(partner).includes(
+            record.get("source_code").toLowerCase()
+         )
       );
       return duplicate ? [...result, duplicate] : result;
    }, []);
@@ -26,7 +33,9 @@ const findDuplicates = (records, partnerList) => {
 const removeDuplicates = (records, duplicates) => {
    return records.filter((record) => {
       return !duplicates.find((duplicate) =>
-         getSourceCodes(duplicate).includes(record.fields.source_code.toLowerCase())
+         getSourceCodes(duplicate).includes(
+            record.get("source_code").toLowerCase()
+         )
       );
    });
 };
@@ -57,14 +66,14 @@ const run = async () => {
    const newRecords = removeDuplicates(approvedRecords, duplicates);
    if (newRecords.length) {
       console.log("New records:");
-      console.log(newRecords.map(({fields}) => fields))
+      console.log(newRecords.map(({ fields }) => fields));
 
       // add new source codes to JSON
       const newPartnerList = [
          ...partnerList,
          ...newRecords.map((record) => ({
-            partnerId: record.fields.source_code,
-            name: record.fields.organization,
+            partnerId: record.get("source_code"),
+            name: record.get("organization"),
          })),
       ];
 
