ba8ce95c65ca748b17e43133b52f8fec3f76041a
billy laing <billy.laing@trestle.us>
2022-08-30 14:33:00 -0400

    [Resources Page] Add basic layout while awaiting for PDF links

--- site/src/components/page-resources/page-resources.tsx
+++ site/src/components/page-resources/page-resources.tsx
@@ -1,7 +1,5 @@
 import {Component, h, Host} from "@stencil/core";
 
-import {ResourceChecklistText} from "../../data";
-
 @Component({
     tag: "page-resources",
     styleUrl: "page-resources.scss",
@@ -13,30 +11,40 @@ export class PageResources {
         return (
             <Host>
                 <h1>Resources</h1>
-                <h2>You’ve signed up to be a poll worker! Now it’s time to do some homework so you’re ready to
-                    serve!</h2>
-                <hr />
-                <div class="checklist">
-                    <h4>Election Day Poll Worker Checklist</h4>
-                    {
-                        ResourceChecklistText.map((info, index) => (
-                            <div class="checklist-item" key={`checklist-item-${index}`}>
-                                <div class="number">{index + 1 < 10 ? "0" : ""}{index + 1}</div>
-                                {info}
-                            </div>
-                        ))
-                    }
-                </div>
-                <hr />
-                <h4>Additional Resources</h4>
                 <p>
-                    Our partners at the Fair Elections Center have put together
-                    <a target="_blank"
-                       rel="noopener"
-                       href="https://www.fairelectionscenter.org/state-specific-resources"
-                    > voting guides for every state.</a> Brush up on your local laws so you feel more confident about how
-                    to answer common voter questions on Election Day.
+                    You've signed up with Power the Polls and completed the poll worker application through your local
+                    elections office—and now you're waiting to hear back. Or perhaps you've been selected as a poll
+                    worker! Maybe you’re still thinking about submitting an application and want to know more before you
+                    do. Wherever you are in the process, here are some resources to help you get ready to serve.
                 </p>
+
+
+                <h2>You’ve signed up to be a poll worker—now what?</h2>
+                <p>
+                    You’ve signed up with Power the Polls—now what? Read here for an overview of your journey to
+                    becoming a poll worker.
+                </p>
+
+                <h2>A Day in the Life of a Poll Worker</h2>
+                <p>
+                    Want to learn a little bit more about what to expect on the average day of a poll worker? Read here
+                    for more.
+                </p>
+
+                <h2>Questions to Ask in Your Official Poll Worker Training</h2>
+                <p>
+                    Your local elections office will schedule an official training for you. Get ready to make the most
+                    of this valuable training time.
+                </p>
+
+                <p>
+                    Please note these supplemental resources do not take the place of the official poll worker training
+                    and resources that your elections office provides. If you have specific questions related to your
+                    service as a poll worker or the rules and resources in your jurisdiction,{" "}
+                    <stencil-route-link url="/search">reach out to your local election
+                        administrator.</stencil-route-link>
+                </p>
+
             </Host>
         );
     }
