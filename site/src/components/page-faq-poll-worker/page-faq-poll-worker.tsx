60dbb716825ae7a16a678d28fe7e3bc17f71a546
Sage Vousé <svouse@gmail.com>
2023-03-30 18:59:40 -0400

    add faq semicolons

--- site/src/components/page-faq-poll-worker/page-faq-poll-worker.tsx
+++ site/src/components/page-faq-poll-worker/page-faq-poll-worker.tsx
@@ -24,7 +24,7 @@ export class PageFaqPollWorker {
                         sectionTitle={sectionTitle}
                         questions={questions}
                     />
-                ))}
+                ))};
          <p><a href="https://www.powerthepolls.org/faq-eligibility" target="_self"> Eligibility FAQs</a><p>
          <p><a href="https://www.powerthepolls.org/faq-application-status" target="_self"> Application Status FAQs</a><p>
       </Host > );
