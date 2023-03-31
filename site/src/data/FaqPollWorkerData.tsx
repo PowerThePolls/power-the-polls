9ba286eb724b9b56973b8808e1929f9a1a1b6889
Sage Vousé <svouse@gmail.com>
2023-03-31 16:48:47 -0400

    refactor subset of eligibility data to poll worker data

--- site/src/data/FaqPollWorkerData.tsx
+++ site/src/data/FaqPollWorkerData.tsx
@@ -4,6 +4,37 @@ import {FunctionalComponent, h} from "@stencil/core";
  * Ordered list of questions and answers for page-faq.tsx, see also app-root.tsx
  */
 const data: { sectionTitle: string, questions: { question: string, answer: FunctionalComponent }[] }[] = [
+        {
+        sectionTitle: "Poll worker responsibilities",
+        questions: [
+            {
+                question: "WHAT DO POLL WORKERS DO?",
+                answer: () => <p>
+                    There are many kinds of jobs for poll workers to do, including helping voters check in, managing
+                    voter lines, troubleshooting equipment, and providing directions and assistance. Poll workers may
+                    also assist with various office duties.
+                </p>,
+            },
+            {
+                question: "IS A POLL WORKER THE SAME AS A POLL WATCHER?",
+                answer: () => <p>
+                    No, these are different roles. Poll workers work for election administrators to help administer the
+                    election. They do things like set up the polling location, check in voters, assist voters with
+                    questions, and troubleshoot any other issues. Poll watchers, which are sometimes also known as poll
+                    observers and poll monitors, are volunteers or staff from an outside organization, political party
+                    or campaign who are certified to observe and monitor election administration. Power the Polls only
+                    recruits poll workers.
+                </p>,
+            },
+            {
+                question: "DO POLL WORKERS NEED TRAINING?",
+                answer: () => <p>
+                    Yes, prior to Election Day, local jurisdictions provide training for all necessary information that
+                    poll workers need.
+                </p>,
+            },
+        ],
+    },
     {
         sectionTitle: "After you apply to be a poll worker",
         questions: [
@@ -74,6 +105,47 @@ const data: { sectionTitle: string, questions: { question: string, answer: Funct
                     Poll workers usually cast their votes early or by mail, but some cast their votes on Election Day.
                     Certain jurisdictions may require poll workers to vote early or via absentee ballot. Your local
                     election official can provide details about how poll workers cast their ballots.
+                </p>,
+            },
+        ],
+    },
+    {
+        sectionTitle: "Compensation and safety",
+        questions: [
+            {
+                question: "ARE POLL WORKERS PAID?",
+                answer: () => <p>
+                    Local jurisdictions will often pay poll workers a stipend for their participation. In most cases,
+                    you will be paid with a check for a day’s worth of work. In some cases, poll working may be
+                    voluntary and not paid. You can find out more about pay in your jurisdiction by <stencil-route-link
+                    url="/search">reaching out directly to the local elections office.</stencil-route-link>
+                </p>,
+            },
+            {
+                question: "I’M CONCERNED ABOUT MY SAFETY. WHAT PRECAUTIONS ARE BEING TAKEN TO PROTECT POLL WORKERS?",
+                answer: () => <p>
+                    Local election authorities across the country know how important safety is at polling sites – not
+                    just for voters but for workers too! If you have specific questions or concerns about steps being
+                    taken in your area, please reach out to your local election officials for more information. We know
+                    many are also doing important work this year to provide training and resources to protect poll
+                    workers and voters. Their contact information can be found once you <stencil-route-link
+                    url="/signup#form">sign up to complete your
+                    application</stencil-route-link>.
+                </p>,
+            },
+            {
+                question: "I HAVE A DISABILITY. WHAT ACCOMODATIONS ARE BEING PROVIDED?",
+                answer: () => <p>
+                    Accommodations vary state to state, so the best place to find information is by contacting your
+                    local election administrator directly. You can find their info <stencil-route-link
+                    url="/search">here</stencil-route-link>.
+                </p>,
+            },
+            {
+                question: "I DON'T WANT TO BE PAID TO BE A POLL WORKER.",
+                answer: () => <p>
+                    If you are not interested in being paid, you can ask your administrator about waiving your wages. If
+                    that is not possible, you can choose to donate it to a charity.
                 </p>,
             },
         ],
