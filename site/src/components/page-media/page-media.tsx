427f7755a02402c2209ba85688e3e5f72a06965e
Sage Vousé <svouse@gmail.com>
2023-05-31 21:48:56 -0400

    fix component name

--- site/src/components/page-media/page-media.tsx
+++ site/src/components/page-media/page-media.tsx
@@ -1,11 +1,10 @@
 import {Component, h, Host} from "@stencil/core";
 
 @Component({
-    tag: "page-impact",
-    styleUrl: "page-impact.scss",
+    tag: "page-media",
     shadow: false,
 })
-export class PageImpact {
+export class PageMedia {
 
     public render() {
         return (
