8b08164180b586a96ff48898739e94fdd13fb14a
Sage Vousé <svouse@gmail.com>
2023-05-31 23:33:58 -0400

    lint

--- site/src/components/page-media/page-media.tsx
+++ site/src/components/page-media/page-media.tsx
@@ -10,15 +10,15 @@ export class PageMedia {
         return (
             <Host>
                 <h1>Press & Media</h1>
-                <h2>Press Releases<h2>
+                <h2>Press Releases</h2>
                     <div class="card">
                     <a href="/assets/documents/25-questions-to-ask.pdf" rel="noopener noreferrer">
-                        <h2>
+                        <h3>
                             Press Release 1
-                        </h2>
+                        </h3>
                         </a>
                     </div>
-                <h1> In the News </h1>
+                <h2> In the News </h2>
                 <div class="card">
                          <a href="https://www.politico.com/newsletters/playbook/2022/11/03/bidens-important-puzzling-democracy-speech-00064817"
                                                                          target="_blank" rel="noopener noreferrer">
