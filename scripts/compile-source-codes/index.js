70597973c9a2b4697ebacde80a6d978c626860ed
Sage Vousé <svouse@gmail.com>
2023-10-02 13:19:15 -0400

    upd

--- scripts/compile-source-codes/index.js
+++ scripts/compile-source-codes/index.js
@@ -2,7 +2,7 @@ const fs = require("fs/promises");
 const Airtable = require("airtable");
 
 const getApprovedRecords = async () => {
-   const base = new Airtable().base('appc14jHeQ2v7FhU9');
+   const base = new Airtable.base('appc14jHeQ2v7FhU9');
 
    const filterByFormula = "{status} = 'Approved'";
    const fields = ["organization", "source_code", "status"];
