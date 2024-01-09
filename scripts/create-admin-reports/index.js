8e1dcc4f3b8ca238fd4de90e8bc5731b50fff5ca
Sage Vousé <svouse@gmail.com>
2024-01-09 14:47:09 -0500

    Create admin report script

--- scripts/create-admin-reports/index.js
+++ scripts/create-admin-reports/index.js
@@ -2,16 +2,19 @@ import fetch, { Headers } from "node-fetch";
 import Airtable from "airtable";
 
 async function getApprovedRecords() {
-   const { AIRTABLE_PARTNERS_BASE } = process.env;
-   const base = new Airtable().base("appc14jHeQ2v7FhU9");
+   const { AIRTABLE_EADMINS_BASE } = process.env;
+   const base = new Airtable().base("appwfzqCbSifnwrme");
    const fields = [
-      "organization",
-      "report_type",
-      "report_emails",
+      "Name",
+      "State",
+      "Jurisdiction Type",
+      "Jurisdiction Name",
+      "Emails",
       "report_frequency",
-      "source_code",
    ];
-   return base("Election Administrators").select({ fields }).all();
+   return base("Election Administrators")
+      .select({fields })
+      .all();
 }
 
 const actionKitURL = "https://ptp.actionkit.com";
@@ -47,11 +50,11 @@ async function callActionKit(path, method = "get", body) {
 function getParams() {
    const params = new URLSearchParams();
    params.set("_limit", "100");
-   params.set("categories__name", "ea_reports");
+   params.set("categories__name", "testing");
    return params.toString();
 }
 
-async function getPartnerReportList() {
+async function getAdminReportList() {
    let response = await callActionKit(`/rest/v1/queryreport?${getParams()}`);
    let reportList = response.objects;
    while (response.meta.next) {
@@ -61,180 +64,36 @@ async function getPartnerReportList() {
    return reportList;
 }
 
-const getSql = (State, Jurisdiction, JurisdictionType) => {
-   if (JurisdictionType === "County") {
-      qaS;
-      const county = Jurisdiction.replace(" County", "");
-      return `SELECT
-   *
-FROM
-(
-SELECT u.first_name
-     , u.last_name
-     , u.email
-     , (SELECT coalesce(group_concat(phone ORDER BY core_phone.id DESC SEPARATOR ', '), '')
-        FROM core_phone
-        WHERE core_phone.user_id = u.id) AS phone
-     , u.city
-     , uf.value AS county
-     , u.state
-     , u.zip
-     , coalesce((SELECT coalesce(group_concat(DISTINCT trim(value) ORDER BY value SEPARATOR ', '), '')
-                 FROM core_action a
-                 JOIN core_actionfield af ON a.id = af.parent_id
-                 WHERE af.name = 'waitlist'
-                   AND a.user_id = u.id), '') AS waitlist
-     , coalesce((SELECT coalesce(group_concat(DISTINCT trim(value) ORDER BY value SEPARATOR ', '), '')
-                 FROM core_action a
-                 JOIN core_actionfield af ON a.id = af.parent_id
-                 WHERE af.name = 'language'
-                   AND a.user_id = u.id), '') AS languages
-     , coalesce((SELECT max(DISTINCT uf.value)
-                 FROM core_userfield uf
-                 WHERE uf.name = 'tech_skills'
-                   AND uf.parent_id = u.id), '') AS tech_skills
-     , (SELECT coalesce(group_concat(DISTINCT trim(value) ORDER BY value SEPARATOR ', '), '')
-        FROM core_action a
-        JOIN core_actionfield af ON a.id = af.parent_id
-        WHERE (af.name = 'travel' AND a.user_id = u.id)
-        ) AS willing_to_travel
-     , (SELECT coalesce(group_concat(DISTINCT trim(value) ORDER BY value SEPARATOR ', '), '')
-        FROM core_action a
-        JOIN core_actionfield af ON a.id = af.parent_id
-        WHERE (af.name= 'travel_distance' AND a.user_id = u.id)) AS potential_travel_distance
-     , (SELECT max(created_at)
-        FROM core_action
-        WHERE user_id = u.id) AS latest_action
-     , (SELECT max(created_at)
-        FROM core_action
-        WHERE user_id = u.id
-          AND page_id = 12) AS latest_signup
-      FROM core_user AS u
-      JOIN core_userfield uf ON u.id = uf.parent_id
-      WHERE lower(u.state) = lower('${State}') AND uf.name = 'county' AND lower(uf.value) = lower('${county}')
-      ORDER BY waitlist DESC, latest_signup DESC
-) as innerTable WHERE waitlist = "Yes"`;
+function getSQL(jurisdictionType, jurisdictionName) {
+   // language=MySQL
+   if (jurisdictionType == "County") {
+      Console.log(
+         `That is a county-- ${jurisdictionType} named ${jurisdictionName}`,
+      );
+      return `That is a county-- ${jurisdictionType} named ${jurisdictionName}`;
+   } else if (jurisdictionType == "City") {
+      Console.log(
+         `That is a county-- ${jurisdictionType} named ${jurisdictionName}`,
+      );
+      return `That is a city-- ${jurisdictionType} named ${jurisdictionName}`;
+   } else {
+      Console.log(
+         `That is a county-- ${jurisdictionType} named ${jurisdictionName}`,
+      );
+      return `That is a state-- ${jurisdictionType} named ${jurisdictionName}`;
    }
-   if (JurisdictionType === "City") {
-      const city = Jurisdiction.replace(" (City)", "").replace(" (city)", "");
-      return `SELECT
-   *
-FROM
-(
-SELECT u.first_name
-     , u.last_name
-     , u.email
-     , (SELECT coalesce(group_concat(phone ORDER BY core_phone.id DESC SEPARATOR ', '), '')
-        FROM core_phone
-        WHERE core_phone.user_id = u.id) AS phone
-     , u.city
-     , uf.value AS county
-     , u.state
-     , u.zip
-     , coalesce((SELECT coalesce(group_concat(DISTINCT trim(value) ORDER BY value SEPARATOR ', '), '')
-                 FROM core_action a
-                 JOIN core_actionfield af ON a.id = af.parent_id
-                 WHERE af.name = 'waitlist'
-                   AND a.user_id = u.id), '') AS waitlist
-     , coalesce((SELECT coalesce(group_concat(DISTINCT trim(value) ORDER BY value SEPARATOR ', '), '')
-                 FROM core_action a
-                 JOIN core_actionfield af ON a.id = af.parent_id
-                 WHERE af.name = 'language'
-                   AND a.user_id = u.id), '') AS languages
-     , coalesce((SELECT max(DISTINCT uf.value)
-                 FROM core_userfield uf
-                 WHERE uf.name = 'tech_skills'
-                   AND uf.parent_id = u.id), '') AS tech_skills
-     , (SELECT coalesce(group_concat(DISTINCT trim(value) ORDER BY value SEPARATOR ', '), '')
-        FROM core_action a
-        JOIN core_actionfield af ON a.id = af.parent_id
-        WHERE (af.name = 'travel' AND a.user_id = u.id)
-        ) AS willing_to_travel
-     , (SELECT coalesce(group_concat(DISTINCT trim(value) ORDER BY value SEPARATOR ', '), '')
-        FROM core_action a
-        JOIN core_actionfield af ON a.id = af.parent_id
-        WHERE (af.name= 'travel_distance' AND a.user_id = u.id)) AS potential_travel_distance
-     , (SELECT max(created_at)
-        FROM core_action
-        WHERE user_id = u.id) AS latest_action
-     , (SELECT max(created_at)
-        FROM core_action
-        WHERE user_id = u.id
-          AND page_id = 12) AS latest_signup
-      FROM core_user AS u
-      JOIN core_userfield uf ON u.id = uf.parent_id
-      WHERE lower(u.state) = lower('${State}') AND uf.name = 'county' AND lower(u.city) = lower('${city}')
-      ORDER BY waitlist DESC, latest_signup DESC
-) as innerTable WHERE waitlist = "Yes"`;
-   }
-   if (JurisdictionType === "State") {
-      return `SELECT
-   *
-FROM
-(
-SELECT u.first_name
-     , u.last_name
-     , u.email
-     , (SELECT coalesce(group_concat(phone ORDER BY core_phone.id DESC SEPARATOR ', '), '')
-        FROM core_phone
-        WHERE core_phone.user_id = u.id) AS phone
-     , u.city
-     , uf.value AS county
-     , u.state
-     , u.zip
-     , coalesce((SELECT coalesce(group_concat(DISTINCT trim(value) ORDER BY value SEPARATOR ', '), '')
-                 FROM core_action a
-                 JOIN core_actionfield af ON a.id = af.parent_id
-                 WHERE af.name = 'waitlist'
-                   AND a.user_id = u.id), '') AS waitlist
-     , coalesce((SELECT coalesce(group_concat(DISTINCT trim(value) ORDER BY value SEPARATOR ', '), '')
-                 FROM core_action a
-                 JOIN core_actionfield af ON a.id = af.parent_id
-                 WHERE af.name = 'language'
-                   AND a.user_id = u.id), '') AS languages
-     , coalesce((SELECT max(DISTINCT uf.value)
-                 FROM core_userfield uf
-                 WHERE uf.name = 'tech_skills'
-                   AND uf.parent_id = u.id), '') AS tech_skills
-     , (SELECT coalesce(group_concat(DISTINCT trim(value) ORDER BY value SEPARATOR ', '), '')
-        FROM core_action a
-        JOIN core_actionfield af ON a.id = af.parent_id
-        WHERE (af.name = 'travel' AND a.user_id = u.id)
-        ) AS willing_to_travel
-     , (SELECT coalesce(group_concat(DISTINCT trim(value) ORDER BY value SEPARATOR ', '), '')
-        FROM core_action a
-        JOIN core_actionfield af ON a.id = af.parent_id
-        WHERE (af.name= 'travel_distance' AND a.user_id = u.id)) AS potential_travel_distance
-     , (SELECT max(created_at)
-        FROM core_action
-        WHERE user_id = u.id) AS latest_action
-     , (SELECT max(created_at)
-        FROM core_action
-        WHERE user_id = u.id
-          AND page_id = 12) AS latest_signup
-      FROM core_user AS u
-      JOIN core_userfield uf ON u.id = uf.parent_id
-      WHERE lower(u.state) = lower('${State}') AND uf.name = 'county'
-      ORDER BY waitlist DESC, latest_signup DESC
-) as innerTable WHERE waitlist = "Yes"`;
-   }
-   return "";
-};
+}
 
 function convertArray(array) {
    return array.map((code) => `'${code.toLowerCase()}'`).join(",");
 }
 
-function isAggregate(partner) {
-   return !partner.get("report_type").startsWith("List");
+function getFrequency(admin) {
+   return admin.get("report_frequency").toLowerCase();
 }
 
-function getFrequency(partner) {
-   return partner.get("report_frequency").toLowerCase();
-}
-
-function hasReportEmails(partner) {
-   return partner.get("report_emails").replace(/ /g, "").length;
+function hasReportEmails(admin) {
+   return admin.get("report_emails").replace(/ /g, "").length;
 }
 
 function sanitizeEmails(emails) {
@@ -242,22 +101,23 @@ function sanitizeEmails(emails) {
 }
 
 function getBody({
-   organization,
-   sourceCodes,
-   isAggregate,
+   admin,
+   State,
+   jurisdictionName,
+   jurisdictionType,
    frequency,
    emails,
 }) {
    return {
-      name: `Power the Polls Report 2023: ${organization}`,
-      short_name: `PowerThePolls-${sourceCodes[0]}-report-updated-fix-2023`,
+      name: `Power the Polls Test Election Admin Report 1: ${organization}`,
+      short_name: `test_admin_0`,
       description: sourceCodes[0],
-      sql: getSQL(sourceCodes, isAggregate),
+      sql: getSQL(sourceCodes, jurisdictionName, jurisdictionType),
       run_every: frequency,
       to_emails: emails.replace(/ /g, ""),
       email_always_csv: true,
       send_if_no_rows: false,
-      categories: ["/rest/v1/reportcategory/18/"],
+      categories: ["/rest/v1/reportcategory/21/"],
    };
 }
 
@@ -266,57 +126,53 @@ async function createReport(reportConfig) {
    await callActionKit("/rest/v1/queryreport/", "post", JSON.stringify(body));
 }
 
-function getReportConfig(partner) {
+function getReportConfig(admin) {
    return {
-      organization: partner.get("organization").trim(),
-      sourceCodes: [partner.get("source_code").trim()],
-      isAggregate: isAggregate(partner),
-      frequency: getFrequency(partner),
-      emails: sanitizeEmails(partner.get("report_emails")),
+      organization: admin.get("organization").trim(),
+      sourceCodes: [admin.get("source_code").trim()],
+      frequency: getFrequency(admin),
+      emails: sanitizeEmails(admin.get("report_emails")),
    };
 }
 
-function logPartners(partners) {
-   const sourceCodes = partners.map((partner) => partner.get("source_code"));
+function logAdmins(admin) {
+   const sourceCodes = admin.map((admin) => admin.get("source_code"));
    console.log(JSON.stringify(sourceCodes, null, 2));
 }
 
-async function createNewReports(approvedPartners, reportList) {
+async function createNewReports(approvedAdmins, reportList) {
    let errorThrown = false;
 
-   const newPartners = approvedPartners.filter((partner) => {
+   const newAdmins = approvedAdmins.filter((admin) => {
       const found = reportList.find(
-         (report) => report.description === partner.get("source_code"),
+         (report) => report.description === admin.get("source_code"),
       );
       return !found;
    });
 
    console.log("New Reports:");
-   logPartners(newPartners);
+   logAdmins(newAdmins);
 
-   for (const partner of newPartners) {
-      // create report for new partners
-      if (hasReportEmails(partner)) {
+   for (const admin of newAdmins) {
+      // create report for new admins
+      if (hasReportEmails(admin)) {
          try {
-            await createReport(getReportConfig(partner));
-            console.log("Report created for: ", partner.get("organization"));
+            await createReport(getReportConfig(admin));
+            console.log("Report created for: ", admin.get("organization"));
          } catch (e) {
             errorThrown = true;
-            console.log("Error processing: ", partner.get("organization"));
+            console.log("Error processing: ", admin.get("organization"));
             console.error(e);
          }
       } else {
-         console.log(
-            "No report emails found for: ",
-            partner.get("organization"),
-         );
+         console.log("No report emails found for: ", admin.get("organization"));
       }
    }
    return errorThrown;
 }
 
-function isModified(partner, report) {
-   const body = getBody(getReportConfig(partner));
+function isModified(admin, report) {
+   const body = getBody(getReportConfig(admin));
 
    const {
       name,
@@ -353,26 +209,26 @@ async function updateReport(reportId, reportConfig) {
    );
 }
 
-async function updateModifiedReports(approvedPartners, reportList) {
+async function updateModifiedReports(approvedAdmins, reportList) {
    let errorThrown = false;
 
-   const modifiedPartners = approvedPartners.reduce((modified, partner) => {
+   const modifiedAdmins = approvedAdmins.reduce((modified, admin) => {
       const report = reportList.find(
-         (report) => report.description === partner.get("source_code"),
+         (report) => report.description === admin.get("source_code"),
       );
-      if (!report || !isModified(partner, report)) {
+      if (!report || !isModified(admin, report)) {
          return modified;
       }
-      partner.report_id = report.id;
-      return [...modified, partner];
+      admin.report_id = report.id;
+      return [...modified, admin];
    }, []);
 
    console.log("Modified Reports:");
-   logPartners(modifiedPartners);
+   logAdmins(modifiedAdmins);
 
-   for (const partner of modifiedPartners) {
+   for (const admin of modifiedAdmins) {
       try {
-         await updateReport(partner.report_id, getReportConfig(partner));
+         await updateReport(admin.report_id, getReportConfig(admin));
       } catch (e) {
          errorThrown = true;
          console.error(e);
@@ -383,11 +239,11 @@ async function updateModifiedReports(approvedPartners, reportList) {
 }
 
 async function run() {
-   // get all approved partners from airtable
+   // get all approved admins from airtable
    const approvedRecords = await getApprovedRecords();
 
-   // get partner reports from ActionKit
-   const reportList = await getPartnerReportList();
+   // get admin reports from ActionKit
+   const reportList = await getAdminReportList();
 
    // create new reports
    const creatErrorThrown = await createNewReports(approvedRecords, reportList);
