c74190e9812737742badc9f9a9a846aadeee88ac
Sage Vousé <svouse@gmail.com>
2024-01-30 11:07:36 -0500

    add full text, remaining image

--- site/src/components/press-release-2024-01-30/press-release-2024-01-30.tsx
+++ site/src/components/press-release-2024-01-30/press-release-2024-01-30.tsx
@@ -134,6 +134,150 @@ export class PagePressRelease20240130 {
                     volunteerism, and helping to ensure election offices have
                     the staff they need throughout 2024.
                 </p>
+
+                <h1>
+                    Power the Polls anuncia un nuevo conjunto de herramientas
+                    bilingües para la contratación de trabajadores electorales
+                </h1>
+                <p>
+                    <i>
+                        Un nuevo recurso en línea, lanzado el Día de Ayuda a
+                        América a Votar 2024, proporciona herramientas para
+                        contratar a trabajadores electorales en 11 idiomas
+                    </i>
+                </p>
+                <p>
+                    <b>30 de enero de 2024 -</b>{" "}
+                    <a href="https://www.powerthepolls.org/">Power the Polls</a>
+                    , la primera iniciativa en su tipo, no partidista, para
+                    reclutar a la próxima generación de trabajadores electorales
+                    que ayuden a garantizar unas elecciones seguras, justas y
+                    protegidas, ha publicado hoy un nuevo{" "}
+                    <a href="https://bit.ly/bilingual-poll-worker-toolkit-pr">
+                        conjunto de herramientas bilingües para la contratación
+                        de trabajadores electorales
+                    </a>
+                    . El conjunto de herramientas, publicado el{" "}
+                    <a href="https://electionline.org/events/help-america-vote-day-2024/">
+                        Día de Ayuda a América a Votar 2024
+                    </a>
+                    , representa el primer paquete de recursos en idiomas de la
+                    organización para apoyar a los socios y administradores
+                    electorales en la contratación de trabajadores electorales
+                    bilingües.
+                </p>
+                <p>
+                    “En Power the Polls, estamos centrados en contratar a la
+                    nueva generación de trabajadores electorales, incluidos
+                    aquellos que tienen la fluidez tecnológica y las habilidades
+                    lingüísticas que los administradores electorales más
+                    necesitan. Este nuevo conjunto de herramientas de
+                    reclutamiento de trabajadores electorales en su idioma
+                    fomenta nuestro compromiso de reclutar trabajadores
+                    electorales bilingües que puedan ayudar a sus vecinos a
+                    votar”, dijo{" "}
+                    <b>
+                        Marta Hanson, directora del programa nacional de Power
+                        the Polls.
+                    </b>{" "}
+                    “Con este conjunto de herramientas, los administradores
+                    electorales y las organizaciones comunitarias pueden
+                    comprometerse más directamente con los hablantes bilingües y
+                    contratarlos, y contribuir a que las elecciones sean más
+                    accesibles para todos los votantes”.
+                </p>
+                <p>
+                    El conjunto de herramientas proporciona textos y gráficos
+                    para las redes sociales en 11 idiomas centrados en la
+                    necesidad de trabajadores electorales para las elecciones de
+                    este año y en cómo inscribirse. Los idiomas incluidos en el
+                    conjunto de herramientas inicial son: árabe, chino
+                    (simplificado), hindi, hmong, japonés, jemer, coreano,
+                    punjabi, español, tagalo y vietnamita, y se añadirán más
+                    antes de las elecciones de noviembre de 2024. De todas las
+                    solicitudes de trabajadores electorales bilingües por parte
+                    de los administradores electorales durante el ciclo de 2022,
+                    estos idiomas fueron los más solicitados. Todos los gráficos
+                    del conjunto de herramientas incluyen también texto
+                    alternativo en su idioma para los lectores de pantalla, con
+                    el fin de mejorar aún más su accesibilidad.
+                </p>
+                <img src="/assets/images/resources/bilingual_toolkit_demo_es.png"></img>
+                <p>
+                    La publicación del conjunto de herramientas refleja la
+                    creciente demanda de trabajadores electorales bilingües para
+                    atender a diversas comunidades de votantes en todo el país.
+                    En las últimas tres décadas, el número de personas en EE.
+                    UU. que hablan en casa un idioma distinto del inglés se ha
+                    triplicado, al pasar de 23,5 millones (aproximadamente 1 de
+                    cada 10) a 67,8 millones (aproximadamente 1 de cada 5),
+                    según{" "}
+                    <a href="https://share.america.gov/united-states-rich-in-languages/">
+                        ShareAmerica y la Oficina del Censo de EE. UU
+                    </a>{" "}
+                    . En respuesta a las peticiones de los administradores
+                    electorales de jurisdicciones de todo el país, Power the
+                    Polls reclutó a trabajadores electorales bilingües que
+                    hablaban más de 25 idiomas en 2022, y la organización tiene
+                    más de 50 idiomas representados en su base de datos de
+                    posibles trabajadores electorales.
+                </p>
+                <p>
+                    Para acceder al conjunto de herramientas,{" "}
+                    <a href="https://bit.ly/bilingual-poll-worker-toolkit-pr">
+                        haga clic aquí
+                    </a>
+                    . Para inscribirse como trabajador electoral u obtener más
+                    información, visite
+                    <a href="http://www.powerthepolls.org">
+                        www.powerthepolls.org
+                    </a>
+                    .
+                </p>
+                <p class="divider">
+                    <b>###</b>
+                </p>
+                <p>
+                    <b>Acerca de Power the Polls</b>
+                    <br />
+                    Power the Polls es una iniciativa no partidista, primera en
+                    su tipo, para contratar a la nueva generación de
+                    trabajadores electorales con el fin de garantizar unas
+                    elecciones seguras y justas para todos los votantes. Lanzada
+                    en junio de 2020 por una coalición de empresas y
+                    organizaciones sin ánimo de lucro, Power the Polls reclutó a
+                    más de 700 000 futuros trabajadores electorales en todo
+                    Estados Unidos durante las elecciones de 2020 y a otros 275
+                    000 trabajadores electorales en 2022. Power the Polls
+                    contrata directamente a los trabajadores electorales, se
+                    coordina con los administradores electorales para
+                    identificar y satisfacer las necesidades de trabajadores
+                    electorales en las comunidades locales, y alberga un centro
+                    de información centralizado para los trabajadores
+                    electorales locales. Para más información sobre Power the
+                    Polls, visite
+                    <a href="http://www.powerthepolls.org">
+                        www.powerthepolls.org
+                    </a>
+                    .
+                </p>
+                <p>
+                    <b>Acerca del Día de Ayuda a América a Votar</b>
+                    <br />
+                    El Día de Ayuda a América a Votar es un día de acción
+                    establecido por la Comisión de Asistencia Electoral de EE.
+                    UU. para animar a la gente de todo el país a participar en
+                    el proceso electoral inscribiéndose para ser trabajador
+                    electoral y una oportunidad para compartir información sobre
+                    cómo registrarse y emitir un voto. Al animar a más personas
+                    a convertirse en trabajadores electorales en sus
+                    comunidades, el Día de Ayuda a América a Votar está
+                    abordando la escasez crítica de trabajadores electorales,
+                    fortaleciendo nuestra república, inspirando un mayor
+                    compromiso cívico y voluntariado, y ayudando a garantizar
+                    que las oficinas electorales cuenten con el personal que
+                    necesitan a lo largo de 2024.
+                </p>
             </Host>
         );
     }
