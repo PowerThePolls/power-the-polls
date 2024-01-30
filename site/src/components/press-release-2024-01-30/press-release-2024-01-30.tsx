2b9c2e2d2703ad82756f8dc55f5450b18bbe9b27
Sage Vousé <svouse@gmail.com>
2024-01-30 10:52:45 -0500

    update text/links

--- site/src/components/press-release-2024-01-30/press-release-2024-01-30.tsx
+++ site/src/components/press-release-2024-01-30/press-release-2024-01-30.tsx
@@ -2,7 +2,7 @@ import { Component, h, Host } from "@stencil/core";
 
 @Component({
     tag: "page-press-release-2024-01-30",
-    styleUrl: "ppage-press-release-2024-01-30.scss",
+    styleUrl: "page-press-release-2024-01-30.scss",
     shadow: false,
 })
 export class PagePressRelease20240130 {
@@ -24,14 +24,22 @@ export class PagePressRelease20240130 {
                     </i>
                 </p>
                 <p>
-                    <b>January 30, 2024 -</b> Power the Polls, the first-of-its-kind,
-                    nonpartisan initiative for recruiting the next generation of
-                    poll workers to help ensure safe, fair, and secure
-                    elections, today released a new Bilingual Poll Worker
-                    Recruitment Toolkit. The toolkit, released on Help America
-                    Vote Day 2024, represents the organization’s first batch of
-                    in-language resources to support partners and election
-                    administrators in recruiting bilingual poll workers.
+                    <b>January 30, 2024 -</b>{" "}
+                    <a href="https://www.powerthepolls.org/">Power the Polls</a>
+                    , the first-of-its-kind, nonpartisan initiative for
+                    recruiting the next generation of poll workers to help
+                    ensure safe, fair, and secure elections, today released a
+                    new{" "}
+                    <a href="https://bit.ly/bilingual-poll-worker-toolkit-pr">
+                        Bilingual Poll Worker Recruitment Toolkit
+                    </a>
+                    . The toolkit, released on{" "}
+                    <a href="https://electionline.org/events/help-america-vote-day-2024/">
+                        Help America Vote Day 2024
+                    </a>
+                    , represents the organization’s first batch of in-language
+                    resources to support partners and election administrators in
+                    recruiting bilingual poll workers.
                 </p>
                 <p>
                     “At Power the Polls, we are focused on recruiting the next
@@ -40,11 +48,15 @@ export class PagePressRelease20240130 {
                     administrators most need. This new in-language poll worker
                     recruitment toolkit furthers our commitment to recruiting
                     bilingual poll workers who can help their neighbors vote,”
-                    said Marta Hanson, National Program Manager for Power the
-                    Polls. “With this toolkit, election administrators and
-                    community organizations can more directly engage with and
-                    recruit bilingual speakers and help make elections more
-                    accessible for every voter.”
+                    said{" "}
+                    <b>
+                        Marta Hanson, National Program Manager for Power the
+                        Polls.
+                    </b>{" "}
+                    “With this toolkit, election administrators and community
+                    organizations can more directly engage with and recruit
+                    bilingual speakers and help make elections more accessible
+                    for every voter.”
                 </p>
                 <p>
                     The toolkit provides social media copy and graphics in 11
@@ -59,23 +71,68 @@ export class PagePressRelease20240130 {
                     toolkit graphics also include in-language alt text for
                     screen readers to help further enhance their accessibility.
                 </p>
+                <img src="/assets/images/resources/questions.png"></img>
                 <p>
                     The release of the toolkit reflects the increasing demand
                     for bilingual poll workers to serve diverse communities of
                     voters nationwide. Over the last three decades, the number
                     of people in the U.S. who speak a language other than
                     English at home has tripled, from 23.5 million (about 1 in
-                    10) to 67.8 million (about 1 in 5), per ShareAmerica and the
-                    U.S. Census Bureau. In response to requests from election
-                    administrators in jurisdictions across the country, Power
-                    the Polls recruited bilingual poll workers speaking over 25
-                    languages in 2022 — and the organization has over 50
-                    languages represented in its database of potential poll
-                    workers.
+                    10) to 67.8 million (about 1 in 5), per{" "}
+                    <a href="https://share.america.gov/united-states-rich-in-languages/">
+                        ShareAmerica and the U.S. Census Bureau.
+                    </a>{" "}
+                    In response to requests from election administrators in
+                    jurisdictions across the country, Power the Polls recruited
+                    bilingual poll workers speaking over 25 languages in 2022 —
+                    and the organization has over 50 languages represented in
+                    its database of potential poll workers.
                 </p>
                 <p>
-                    To access the toolkit, click here. To sign up to be a poll
-                    worker or learn more, please visit www.powerthepolls.org.
+                    To access the toolkit,{" "}
+                    <a href="https://bit.ly/bilingual-poll-worker-toolkit-pr">
+                        click here
+                    </a>
+                    . To sign up to be a poll worker or learn more, please visit
+                    <a href="http://www.powerthepolls.org">
+                        www.powerthepolls.org
+                    </a>
+                    .
+                </p>
+                <p class="divider">###</p>
+                <p>
+                    <b>About Power the Polls</b>
+                    <br />
+                    Power the Polls is a first-of-its-kind, nonpartisan
+                    initiative to recruit the next generation of poll workers to
+                    ensure safe and fair elections for all voters. Launched in
+                    June 2020 by a coalition of businesses and nonprofits, Power
+                    the Polls recruited over 700,000 prospective poll workers
+                    across the United States during the 2020 elections and an
+                    additional 275,000 poll workers in 2022. Power the Polls
+                    directly recruits poll workers, coordinates with election
+                    administrators to identify and meet poll worker needs in
+                    local communities, and houses a centralized hub for local
+                    poll worker information. For more on Power the Polls, visit
+                    <a href="http://www.powerthepolls.org">
+                        www.powerthepolls.org
+                    </a>
+                    .
+                </p>
+                <p>
+                    <b>About Help America Vote Day</b>
+                    <br />
+                    Help America Vote Day is a day of action established by the
+                    U.S. Election Assistance Commission to encourage people
+                    across the U.S. to get involved in the electoral process by
+                    signing up to be a poll worker and an opportunity to share
+                    information on how to register and cast a ballot. By
+                    encouraging more people to become poll workers in their
+                    communities, Help America Vote Day is addressing the
+                    critical shortage of poll workers, strengthening our
+                    republic, inspiring greater civic engagement and
+                    volunteerism, and helping to ensure election offices have
+                    the staff they need throughout 2024.
                 </p>
             </Host>
         );
