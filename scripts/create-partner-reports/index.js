12368cb26b98d72cf52179f48b0358a6e0e41a84
Sage Vousé <svouse@gmail.com>
2024-03-04 18:01:55 -0500

    change actionkit query to continue debug of partner script

--- scripts/create-partner-reports/index.js
+++ scripts/create-partner-reports/index.js
@@ -53,14 +53,11 @@ function getParams() {
 }
 
 async function getPartnerReportList() {
-   let queryResponse = await callActionKit(`/rest/v1/queryreport?${getParams()}`);
-   let dashboardResponse = await callActionKit(`/rest/v1/dashboardreport?${getParams()}`);
-   let reportList = [queryResponse.objects, dashboardResponse.objects];
-   while (queryResponse.meta.next || dashboardResponse.meta.next) {
-      queryResponse = await callActionKit(queryResponse.meta.next);
-      dashboardResponse = await callActionKit(dashboardResponse.meta.next);
-      reportList = reportList.concat(queryResponse.objects);
-      reportList = reportList.concat(dashboardResponse.objects);
+    let response = await callActionKit(`/rest/v1/queryreport?${getParams()}`);
+    let reportList = response.objects;
+    while (response.meta.next) {
+       response = await callActionKit(response.meta.next);
+       reportList = reportList.concat(response.objects);
    }
    return reportList;
 }
