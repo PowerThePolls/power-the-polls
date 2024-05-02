9b012f7e17752755fb4c91b226fb88aea3f5fa64
Sage Vousé <svouse@gmail.com>
2024-05-02 14:07:20 -0400

    make updates to padding, add co-chair section

--- site/src/components/page-advisory/page-advisory.tsx
+++ site/src/components/page-advisory/page-advisory.tsx
@@ -14,7 +14,7 @@ export class PageAdvisory {
         <header aria-label="Advisory Council Page Header">
           <h1>{this.pageTitle || ""}</h1>
         </header>
-
+        <div class="advisory-council-page">
         <main role="main" aria-label="Advisory Council Page Main Content">
           <section aria-label="Council Description">
             <p class="council-description">
@@ -23,81 +23,90 @@ export class PageAdvisory {
           </section>
 
           <section aria-label="Council Members">
+          <div>
+            <h2>Co-Chairs</h2>
+          </div>
+
+            <div class="co-chair-container">
+                  <div class="council-content">
+                    <stencil-route-link url="/council-member/Kerry Washington">
+                      <img
+                        class="co-chair"
+                        src="/assets/images/council_headshots/cropped_headshots/KerryWashington_1_cropped.jpg"
+                        alt="Headshot of Kerry Washington"
+                      />
+                      <p class="council-name">Kerry Washington</p>
+                      <p class="council-title">Actress, Producer/Director, Activist</p>
+                    </stencil-route-link>
+                  </div>
+                  <div class="council-content">
+                    <stencil-route-link url="/council-member/Adam Kinzinger">
+                      <img
+                        class="co-chair"
+                        src="/assets/images/council_headshots/cropped_headshots/RepAdamKinzinger_APPROVED_cropped.jpg"
+                        alt="Headshot of Rep. Adam Kinzinger"
+                      />
+                      <p class="council-name">Rep. Adam Kinzinger</p>
+                      <p class="council-title">Former Member of Congress (R-IL)</p>
+                    </stencil-route-link>
+                  </div>
+            </div>
             <h2>Members</h2>
             <div class="council-container">
-              <div class="council-content">
-                <stencil-route-link url="/council-member/Kerry Washington">
-                  <img
-                    class="council-headshot"
-                    src="/assets/images/council_headshots/KerryWashington_1.png"
-                    alt="Headshot of Kerry Washington"
-                  />
-                  <p class="council-name">Kerry Washington</p>
-                  <p class="council-title">Actress, Producer/Director, Activist</p>
-                </stencil-route-link>
+                <div class="council-content">
+                  <stencil-route-link url="/council-member/Francisco Aguilar">
+                    <img
+                      class="council-headshot"
+                      src="/assets/images/council_headshots/cropped_headshots/FranciscoAguilar_official_cropped.jpg"
+                      alt="Headshot of Francisco Aguilar"
+                    />
+                    <p class="council-name">Francisco Aguilar</p>
+                    <p class="council-title">Nevada Secretary of State</p>
+                  </stencil-route-link>
+                </div>
+                <div class="council-content">
+                  <stencil-route-link url="/council-member/Brad Raffensperger">
+                    <img
+                      class="council-headshot"
+                      src="/assets/images/council_headshots/cropped_headshots/BradRaffensperger_official_cropped.jpg"
+                      alt="Headshot of Brad Raffensperger"
+                    />
+                    <p class="council-name">Brad Raffensperger</p>
+                    <p class="council-title">Georgia Secretary of State</p>
+                  </stencil-route-link>
               </div>
+
+
               <div class="council-content">
-                <stencil-route-link url="/council-member/Adam Kinzinger">
-                  <img
-                    class="council-headshot"
-                    src="/assets/images/council_headshots/RepAdamKinzinger_APPROVED.jpeg"
-                    alt="Headshot of Rep. Adam Kinzinger"
-                  />
-                  <p class="council-name">Rep. Adam Kinzinger</p>
-                  <p class="council-title">Former Member of Congress (R-IL)</p>
-                </stencil-route-link>
-              </div>
-              <div class="council-content">
-                <stencil-route-link url="/council-member/Francisco Aguilar">
-                  <img
-                    class="council-headshot"
-                    src="/assets/images/council_headshots/FranciscoAguilar_official.jpg"
-                    alt="Headshot of Francisco Aguilar"
-                  />
-                  <p class="council-name">Francisco Aguilar</p>
-                  <p class="council-title">Nevada Secretary of State</p>
-                </stencil-route-link>
-              </div>
-              <div class="council-content">
-                <stencil-route-link url="/council-member/Brad Raffensperger">
-                  <img
-                    class="council-headshot"
-                    src="/assets/images/council_headshots/BradRaffensperger_official.jpeg"
-                    alt="Headshot of Brad Raffensperger"
-                  />
-                  <p class="council-name">Brad Raffensperger</p>
-                  <p class="council-title">Georgia Secretary of State</p>
-                </stencil-route-link>
-              </div>
+                  <stencil-route-link url="/council-member/Rye Barcott">
+                    <img
+                      class="council-headshot"
+                      src="/assets/images/council_headshots/cropped_headshots/RyeBarcott_APPROVED_cropped.jpg"
+                      alt="Headshot of Rye Barcott"
+                    />
+                    <p class="council-name">Rye Barcott</p>
+                    <p class="council-title">Co-founder, With Honor</p>
+                  </stencil-route-link>
+                </div>
+                <div class="council-content">
+                  <stencil-route-link url="/council-member/Arndrea Waters King">
+                    <img
+                      class="council-headshot"
+                      src="/assets/images/council_headshots/cropped_headshots/Arndrea Waters King_official_cropped.jpg"
+                      alt="Headshot of Arndrea Waters King"
+                    />
+                    <p class="council-name">Arndrea Waters King</p>
+                    <p class="council-title">President, Drum Major Institute</p>
+                  </stencil-route-link>
+                </div>
             </div>
+
             <div class="council-container">
-              <div class="council-content">
-                <stencil-route-link url="/council-member/Rye Barcott">
-                  <img
-                    class="council-headshot"
-                    src="/assets/images/council_headshots/RyeBarcott_official.jpg"
-                    alt="Headshot of Rye Barcott"
-                  />
-                  <p class="council-name">Rye Barcott</p>
-                  <p class="council-title">Co-founder, With Honor</p>
-                </stencil-route-link>
-              </div>
-              <div class="council-content">
-                <stencil-route-link url="/council-member/Arndrea Waters King">
-                  <img
-                    class="council-headshot"
-                    src="/assets/images/council_headshots/Arndrea Waters King_official.jpeg"
-                    alt="Headshot of Arndrea Waters King"
-                  />
-                  <p class="council-name">Arndrea Waters King</p>
-                  <p class="council-title">President, Drum Major Institute</p>
-                </stencil-route-link>
-              </div>
               <div class="council-content">
                 <stencil-route-link url="/council-member/Santiago Mayer">
                   <img
                     class="council-headshot"
-                    src="/assets/images/council_headshots/SantiagoMayer_1.jpeg"
+                    src="/assets/images/council_headshots/cropped_headshots/SantiagoMayer_1_cropped.jpg"
                     alt="Headshot of Santiago Mayer"
                   />
                   <p class="council-name">Santiago Mayer</p>
@@ -108,20 +117,19 @@ export class PageAdvisory {
                 <stencil-route-link url="/council-member/Desmond Meade">
                   <img
                     class="council-headshot"
-                    src="/assets/images/council_headshots/DesmondMeade_APPROVED.jpeg"
+                    src="/assets/images/council_headshots/cropped_headshots/DesmondMeade_APPROVED_cropped.jpg"
                     alt="Headshot of Desmond Meade"
                   />
                   <p class="council-name">Desmond Meade</p>
                   <p class="council-title">President and Executive Director, Florida Rights Restoration Coalition</p>
                 </stencil-route-link>
               </div>
-            </div>
-            <div class="council-container">
+
               <div class="council-content">
                 <stencil-route-link url="/council-member/Ashley Spillane">
                   <img
                     class="council-headshot"
-                    src="/assets/images/council_headshots/AshleySpillane_APPROVED.jpeg"
+                    src="/assets/images/council_headshots/cropped_headshots/AshleySpillane_APPROVED_cropped.jpg"
                     alt="Headshot of Ashley Spillane"
                   />
                   <p class="council-name">Ashley Spillane</p>
@@ -132,18 +140,20 @@ export class PageAdvisory {
                 <stencil-route-link url="/council-member/Gabriel Sterling">
                   <img
                     class="council-headshot"
-                    src="/assets/images/council_headshots/GabrielSterling_official.jpeg"
+                    src="/assets/images/council_headshots/cropped_headshots/GabrielSterling_official_cropped.jpg"
                     alt="Headshot of Gabriel Sterling"
                   />
                   <p class="council-name">Gabriel Sterling</p>
                   <p class="council-title">Chief Operating Officer, Office of Georgia Secretary of State</p>
                 </stencil-route-link>
               </div>
+            </div>
+            <div class="council-container">
               <div class="council-content">
                 <stencil-route-link url="/council-member/Charlie Sykes">
                   <img
                     class="council-headshot"
-                    src="/assets/images/council_headshots/CharlieSykes_official.jpeg"
+                    src="/assets/images/council_headshots/cropped_headshots/CharlieSykes_official_cropped.jpg"
                     alt="Headshot of Charlie Sykes"
                   />
                   <p class="council-name">Charlie Sykes</p>
@@ -154,16 +164,17 @@ export class PageAdvisory {
                 <stencil-route-link url="/council-member/Tiffany Yu">
                   <img
                     class="council-headshot"
-                    src="/assets/images/council_headshots/TiffanyYu_APPROVED.jpg"
+                    src="/assets/images/council_headshots/cropped_headshots/TiffanyYu_APPROVED 1_cropped.jpg"
                     alt="Headshot of Tiffany Yu"
                   />
                   <p class="council-name">Tiffany Yu</p>
-                  <p class="council-title">Founder and Author, Diversability</p>
+                  <p class="council-title">Author and Founder, Diversability</p>
                 </stencil-route-link>
               </div>
             </div>
           </section>
         </main>
+      </div>
       </Host>
     );
   }
